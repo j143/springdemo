521d44b8ec608fdaeed0ba5969d8cad43ea0cec4
Janardhan Pulivarthi <j143@protonmail.com>
2022-02-08 16:02:02 +0530

    add web pages

--- src/main/java/page/janardhan/demo/GreetingController.java
+++ src/main/java/page/janardhan/demo/GreetingController.java
@@ -1,6 +1,15 @@
 package page.janardhan.demo;
 
-import
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class GreetingController {
+
+    @GetMapping
+    public String greeting(String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
