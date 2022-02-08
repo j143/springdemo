5546a63fc4c9b1cdbd0e5850c8edc693e53ad1b7
Janardhan Pulivarthi <j143@protonmail.com>
2022-02-08 16:18:23 +0530

    fix /greeting mapping, add Greeting class

---
+++ src/main/java/page/janardhan/demo/Greeting.java
@@ -0,0 +1,20 @@
+package page.janardhan.demo;
+
+public class Greeting {
+    private final long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
--- src/main/java/page/janardhan/demo/GreetingController.java
+++ src/main/java/page/janardhan/demo/GreetingController.java
@@ -5,10 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class GreetingController {
 
-    @GetMapping
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
