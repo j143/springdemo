819c78c7b84019f71e9b46bde5d9b0c0c5efdd21
Janardhan Pulivarthi <j143@protonmail.com>
2022-02-08 16:08:26 +0530

    fix parameters for index.html

--- src/main/java/page/janardhan/demo/GreetingController.java
+++ src/main/java/page/janardhan/demo/GreetingController.java
@@ -3,12 +3,13 @@ package page.janardhan.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
     @GetMapping
-    public String greeting(String name, Model model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
