627c383372b8ed60e57c1d9ac72701a0bc3e2e19
Janardhan Pulivarthi <j143@protonmail.com>
2022-02-09 12:35:07 +0530

    generate ascii docs and test mappings

--- src/main/java/page/janardhan/demo/GreetingController.java
+++ src/main/java/page/janardhan/demo/GreetingController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -14,6 +16,11 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/simple-message")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello, World");
+    }
 
     @CrossOrigin(origins="http://localhost:8080")
     @GetMapping("/greeting")
--- src/test/java/page/janardhan/demo/WebLayerTest.java
+++ src/test/java/page/janardhan/demo/WebLayerTest.java
@@ -3,6 +3,7 @@ package page.janardhan.demo;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.*;
@@ -11,20 +12,26 @@ import static org.hamcrest.Matchers.containsString;
 
 import static
         org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(GreetingController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class WebLayerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/simple-message")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")))
+                .andDo(document("home"));
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, World")));
+        this.mockMvc.perform(get("/greeting-javaconfig")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello")));
     }
 }
