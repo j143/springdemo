09e82f9dd0733a1255b394cd737670ed55b679b6
Janardhan Pulivarthi <j143@protonmail.com>
2022-02-09 09:09:19 +0530

    remote DemoApplication, add WebLayerTest

---
+++ src/main/java/page/janardhan/demo/WebLayerTest.java
@@ -0,0 +1,30 @@
+package page.janardhan.demo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.*;
+
+import static org.hamcrest.Matchers.containsString;
+
+import static
+        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(GreetingController.class)
+public class WebLayerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
